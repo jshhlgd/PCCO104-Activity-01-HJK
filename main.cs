39b20d1c9af931e9390db4f7cb41aeb1500a2dca
jshhlgd <158656874+jshhlgd@users.noreply.github.com>
2024-02-03 03:21:51 +0000

    Added SecondName

--- main.cs
+++ main.cs
@@ -3,9 +3,10 @@ using System;
 class Program {
   public static void Main (string[] args) {
     string firstName = "Josh";
+    string secondName = "Kevin";
     string lastName = "Holgado";
     int age = 13;
-    Console.WriteLine ($"Hello World {firstName} {lastName} Age: {age}");
-    Console.WriteLine ("Hello World " + firstName + " " + lastName + " Age: " + age);
+    Console.WriteLine ($"Hello World {firstName} {secondName} {lastName} Age: {age}");
+    Console.WriteLine ("Hello World " + firstName + " " + secondName + " " + lastName + " Age: " + age);
   }
 }
